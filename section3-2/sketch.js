fa45a1ac6564952d88ffcb146f1ad791ad2da7df
YMiku-12 <cieletoile216@gmail.com>
2021-11-06 18:43:09 +0900

    change 3-2 after class

--- section3-2/sketch.js
+++ section3-2/sketch.js
@@ -14,7 +14,13 @@ function setup(){
 function draw(){
   background(160, 192, 255);
   ellipse(x, y, 20, 20);
-  x += vx;
+
+  if(keyIsDown(LEFT_ARROW)){ x -= 5; }
+  if(keyIsDown(RIGHT_ARROW)){ x += 5; }
+  if(y >= height - 10 && keyIsDown(" ".charCodeAt(0))){
+    vy = -20;
+  }
+
   y += vy;
 
   // 重力（コメント機能でオンオフ切り替えて実行してみましょう）
@@ -26,9 +32,7 @@ function draw(){
   // if(y > height){ y = 0; }
   // if(y < 0){ y = height; }
 
-　// 端の処理パターン (2) 跳ね返る
-  if(x < 0 || x > width){ vx = -1 * vx; }
-  if(y > height){ vy = -1 * vy; }
+  if(y > height -10){ vy = 0; }
   x = constrain(x, 0, width);
   y = constrain(y, 0, height);
 }
